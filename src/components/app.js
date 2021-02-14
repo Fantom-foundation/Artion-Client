4a2c6cbba8e538c16f212ee2cdd93e0df1e4e556
tommy <tommychan4241@gmail.com>
2021-02-14 07:25:55 -0800

    set up eslint & prettier, adding header

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
-import React, { useRef, useEffect } from 'react';
-import { observer, inject } from 'mobx-react';
+import React, { useRef, useEffect } from "react";
+import { observer, inject } from "mobx-react";
+import { BrowserRouter } from "react-router-dom";
 
-import Layout from './layout';
-import Toolbar from './toolbar';
-import { Container, Canvas } from './style';
-import { isMobile } from 'utils/userAgent';
+import Layout from "./layout";
+import Toolbar from "./toolbar";
+import NiftyHeader from "./header";
+import { Container, Canvas } from "./style";
+import { isMobile } from "utils/userAgent";
+import { faPray } from "@fortawesome/free-solid-svg-icons";
 
 const App = ({ paintStore, fullScreen }) => {
   const canvasRef = useRef(null);
@@ -13,23 +16,28 @@ const App = ({ paintStore, fullScreen }) => {
   useEffect(() => paintStore.initialize(canvasRef.current), [paintStore]);
 
   return (
-    <Layout>
-      <Container full={fullScreen} isMobile={isMobile}>
-        <Canvas
-          ref={canvasRef}
-          onMouseDown={start}
-          onMouseMove={draw}
-          onMouseUp={stop}
-          onMouseOut={stop}
-          onTouchStart={start}
-          onTouchMove={draw}
-          onTouchEnd={stop}
-          onTouchCancel={stop}
-        />
-        <Toolbar />
-      </Container>
-    </Layout>
+    <div>
+      <NiftyHeader></NiftyHeader>
+      <BrowserRouter>
+        <Layout>
+          <Container full={fullScreen} isMobile={isMobile}>
+            <Canvas
+              ref={canvasRef}
+              onMouseDown={start}
+              onMouseMove={draw}
+              onMouseUp={stop}
+              onMouseOut={stop}
+              onTouchStart={start}
+              onTouchMove={draw}
+              onTouchEnd={stop}
+              onTouchCancel={stop}
+            />
+            <Toolbar />
+          </Container>
+        </Layout>
+      </BrowserRouter>
+    </div>
   );
 };
 
-export default inject('paintStore')(observer(App));
+export default inject("paintStore")(observer(App));
