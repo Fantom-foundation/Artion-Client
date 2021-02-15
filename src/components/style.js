45cec8d50c1178c9e2746360c8f89d681903db39
tommy <tommychan4241@gmail.com>
2021-02-15 06:50:54 -0800

    add pencil icon on hover

--- src/components/style.js
+++ src/components/style.js
@@ -17,12 +17,13 @@ const setDimensions = (full, isMobile) => {
       return css`
         height: 640px;
         width: 640px;
-        transform: translate(-50%, -50%);
+        transform: translate(-100%, -50%);
         position: fixed;
         top: 50%;
         left: 50%;
-        // border: 1px solid black;
         box-shadow: 3px 3px 5px 6px #ccc;
+        cursor: url(http://www.javascriptkit.com/dhtmltutors/cursor-hand.gif),
+          auto;
       `;
   }
 };
