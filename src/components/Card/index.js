91dc6ce2adfc69207f36a870b9eee6386f0ef858
tommy <tommychan4241@gmail.com>
2021-03-19 02:56:23 +0800

    styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,109 +1,49 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Skeleton from '@material-ui/lab/Skeleton';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    card: {
-      padding: '12px',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  })
-);
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 100,
+    backgroundSize: 'contain',
+  },
+});
 
-const BaseCard = ({ index, style, data }) => {
+const BaseCard = ({ style }) => {
   const classes = useStyles();
-  const loading = data[index] === null;
 
   return (
     <Box style={style} className={classes.root}>
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            loading ? (
-              <Skeleton
-                animation="wave"
-                variant="circle"
-                width={40}
-                height={40}
-              />
-            ) : (
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                R
-              </Avatar>
-            )
-          }
-          action={
-            loading ? null : (
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            )
-          }
-          title={
-            loading ? (
-              <Skeleton
-                animation="wave"
-                height={10}
-                width="80%"
-                style={{ marginBottom: 6 }}
-              />
-            ) : (
-              'Shrimp and Chorizo Paella'
-            )
-          }
-          subheader={
-            loading ? (
-              <Skeleton animation="wave" height={10} width="40%" />
-            ) : (
-              'March 20, 2020'
-            )
-          }
-        />
-        <CardContent>
-          {loading ? (
-            <>
-              <Skeleton
-                animation="wave"
-                height={10}
-                style={{ marginBottom: 6 }}
-              />
-              <Skeleton animation="wave" height={10} width="80%" />
-            </>
-          ) : (
-            <Typography variant="body2" color="textSecondary" component="p">
-              {data[index]}
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://gateway.pinata.cloud/ipfs/QmfKZXhGzPVYLu7YN5GrZnNitpgL8p3ADojuy1rPpLc5TX"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Cute
             </Typography>
-          )}
-        </CardContent>
-        <CardActions disableSpacing>
-          {loading ? null : (
-            <>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-            </>
-          )}
+            <Typography variant="body2" color="textSecondary" component="p">
+              This is a cute girl based NFT
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Details
+          </Button>
         </CardActions>
       </Card>
     </Box>
