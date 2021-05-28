71792693e99cf98a13df92bffe695820a4d25051
akame007 <acame007@gmail.com>
2021-05-28 16:07:30 +1000

    fix issues

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -6,7 +6,7 @@ import HelpIcon from '@material-ui/icons/Help';
 
 import styles from './styles.module.scss';
 
-const Panel = ({ icon, title, fixed, children }) => {
+const Panel = ({ title, fixed, children }) => {
   const [open, setOpen] = useState(!!fixed);
 
   const handleOpen = () => {
@@ -15,16 +15,17 @@ const Panel = ({ icon, title, fixed, children }) => {
     }
   };
 
-  const Icon = icon;
-
   return (
     <div className={styles.container}>
       <div className={styles.header} onClick={handleOpen}>
-        <div className={styles.headerLeft}>
-          {Icon && <Icon className={styles.icon} />}
-          <span className={styles.title}>{title}</span>
-        </div>
-        {fixed ? <HelpIcon /> : open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        <span className={styles.title}>{title}</span>
+        {fixed ? (
+          <HelpIcon className={styles.icon} />
+        ) : open ? (
+          <ExpandLessIcon className={styles.icon} />
+        ) : (
+          <ExpandMoreIcon className={styles.icon} />
+        )}
       </div>
       <div className={cx(styles.body, open ? styles.open : null)}>
         {children}
