e4f3561e53871a051d1867a644d2c0c2ebbc7b0a
akame007 <acame007@gmail.com>
2021-06-10 02:56:01 +1000

    update api endpoint

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -173,7 +173,7 @@ const NiftyHeader = ({ light }) => {
         },
       } = await axios({
         method: 'post',
-        url: `https://api0.artion.io/info/searchNames`,
+        url: `https://api.artion.io/info/searchNames`,
         data: JSON.stringify({ name: word }),
         headers: {
           'Content-Type': 'application/json',
