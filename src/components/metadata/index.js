125505bc2bd737940e18a4baaa779068534da560
akame007 <acame007@gmail.com>
2021-06-10 23:25:43 +1000

    update notification message for mint

--- src/components/metadata/index.js
+++ src/components/metadata/index.js
@@ -16,7 +16,6 @@ import { BigNumber, ethers } from 'ethers';
 
 import { FantomNFTConstants } from 'constants/smartcontracts/fnft.constants';
 import SCHandlers from 'utils/sc.interaction';
-import SystemConstants from 'constants/system.constants';
 
 import toast from 'utils/toast';
 import WalletUtils from 'utils/wallet';
@@ -178,14 +177,10 @@ const Metadata = () => {
       toast('info', 'You are not connected to Fantom Opera Network');
       return;
     }
-    // only when the user has more than 1k ftms on the wallet
-    let balance = await WalletUtils.checkBalance(account);
+    const balance = await WalletUtils.checkBalance(account);
 
-    if (balance < SystemConstants.FMT_BALANCE_LIMIT) {
-      toast(
-        'custom',
-        `Your balance should be at least ${SystemConstants.FMT_BALANCE_LIMIT} ftm to mint an NFT`
-      );
+    if (balance < 5) {
+      toast('custom', `Your balance should be at least 5 FTM to mint an NFT`);
       return;
     }
 
