dac3d92f29829f28e6725fff4f492852a4426ed0
akame007 <acame007@gmail.com>
2021-06-15 20:22:13 +1000

    remove image edit panel

--- src/components/PaintBoard/index.js
+++ src/components/PaintBoard/index.js
@@ -7,7 +7,6 @@ import axios from 'axios';
 import { BigNumber, ethers } from 'ethers';
 import { useDropzone } from 'react-dropzone';
 
-import WarningIcon from '@material-ui/icons/Warning';
 import CloseIcon from '@material-ui/icons/Close';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -16,19 +15,12 @@ import InfoIcon from '@material-ui/icons/Info';
 
 import HeaderActions from 'actions/header.actions';
 import Header from 'components/header';
-import ImageEditor from 'components/ImageEditor';
-import MintModeModal from 'components/MintModeModal';
 import { calculateGasMargin } from 'utils';
 import showToast from 'utils/toast';
 import WalletUtils from 'utils/wallet';
 import SCHandlers from 'utils/sc.interaction';
 import { API_URL } from 'api';
 import { FantomNFTConstants } from 'constants/smartcontracts/fnft.constants';
-import useWindowDimensions from 'hooks/useWindowDimensions';
-
-import whiteTheme from './white-theme';
-
-import transparentBackground from './transparent.png';
 
 import 'tui-image-editor/dist/tui-image-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
@@ -47,14 +39,9 @@ const PaintBoard = () => {
 
   const { account, chainId } = useWeb3React();
 
-  const ref = useRef();
   const imageRef = useRef();
 
-  const [modeSelecting, setModeSelecting] = useState(false);
-  const [mode, setMode] = useState(0);
-  const [imageEditor, setImageEditor] = useState(null);
   const [image, setImage] = useState(null);
-  const { width } = useWindowDimensions();
 
   const [name, setName] = useState('');
   const [symbol, setSymbol] = useState('');
@@ -71,22 +58,9 @@ const PaintBoard = () => {
   );
   const authToken = useSelector(state => state.ConnectWallet.authToken);
 
-  const init = () => {
-    if (width > 1150) {
-      setModeSelecting(true);
-    }
-  };
-
   useEffect(() => {
     dispatch(HeaderActions.toggleSearchbar(false));
-    init();
   }, []);
-
-  useEffect(() => {
-    if (ref.current) {
-      setImageEditor(ref.current.getInstance());
-    }
-  }, [ref.current]);
 
   const onDrop = useCallback(acceptedFiles => {
     setImage(acceptedFiles[0]);
@@ -119,19 +93,8 @@ const PaintBoard = () => {
     });
   };
 
-  const resetBoard = () => {
-    if (!imageEditor) return;
-
-    imageEditor.loadImageFromURL(transparentBackground, 'image');
-  };
-
   const validateMetadata = () => {
-    return (
-      name !== '' &&
-      symbol !== '' &&
-      account !== '' &&
-      (mode === 0 ? image : imageEditor?.toDataURL())
-    );
+    return name !== '' && symbol !== '' && account !== '' && image;
   };
 
   const resetMintingStatus = () => {
@@ -171,12 +134,8 @@ const PaintBoard = () => {
     }
 
     let formData = new FormData();
-    if (mode === 0) {
-      const base64 = await imageToBase64();
-      formData.append('image', base64);
-    } else {
-      formData.append('image', imageEditor.toDataURL());
-    }
+    const base64 = await imageToBase64();
+    formData.append('image', base64);
     formData.append('name', name);
     formData.append('account', account);
     formData.append('description', description);
@@ -224,7 +183,6 @@ const PaintBoard = () => {
         setLastMintedTkId(mintedTkId.toNumber());
 
         showToast('success', 'New NFT item minted!');
-        resetBoard();
         removeImage();
         setName('');
         setSymbol('');
@@ -238,178 +196,129 @@ const PaintBoard = () => {
     resetMintingStatus();
   };
 
-  const options = {
-    includeUI: {
-      loadImage: {
-        path: transparentBackground,
-        name: 'image',
-      },
-      theme: whiteTheme,
-      menuBarPosition: 'right',
-      uiSize: {
-        height: '100%',
-        width: '100%',
-      },
-      draw: {
-        color: '#00a9ff',
-        opacity: 1.0,
-      },
-    },
-    cssMaxWidth: 512,
-    cssMaxHeight: 512,
-  };
-
   return (
     <div className={styles.container}>
-      <Header light onCreate={init} />
+      <Header light />
       <div className={styles.body}>
-        {mode === 0 || width > 1150 ? (
-          <>
-            <div className={styles.board}>
-              {mode === 0 ? (
-                <div {...getRootProps({ className: styles.uploadCont })}>
-                  <input {...getInputProps()} ref={imageRef} />
-                  {image ? (
-                    <>
-                      <img
-                        className={styles.image}
-                        src={URL.createObjectURL(image)}
-                      />
-                      <div className={styles.overlay}>
-                        <CloseIcon
-                          className={styles.remove}
-                          onClick={removeImage}
-                        />
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div className={styles.uploadtitle}>
-                        Drop files here or&nbsp;
-                        <span
-                          className={styles.browse}
-                          onClick={() => imageRef.current?.click()}
-                        >
-                          browse
-                        </span>
-                      </div>
-                      <div className={styles.uploadsubtitle}>
-                        PNG, GIF Max 30mb.
-                      </div>
-                    </>
-                  )}
+        <div className={styles.board}>
+          <div {...getRootProps({ className: styles.uploadCont })}>
+            <input {...getInputProps()} ref={imageRef} />
+            {image ? (
+              <>
+                <img
+                  className={styles.image}
+                  src={URL.createObjectURL(image)}
+                />
+                <div className={styles.overlay}>
+                  <CloseIcon className={styles.remove} onClick={removeImage} />
                 </div>
-              ) : (
-                <ImageEditor ref={ref} {...options} />
-              )}
-            </div>
-            <div className={styles.panel}>
-              <div className={styles.formGroup}>
-                <p className={styles.formLabel}>Name</p>
-                <input
-                  className={styles.formInput}
-                  maxLength={20}
-                  placeholder="Name"
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                  disabled={isMinting}
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <p className={styles.formLabel}>Symbol</p>
-                <input
-                  className={styles.formInput}
-                  maxLength={20}
-                  placeholder="Symbol"
-                  value={symbol}
-                  onChange={e => setSymbol(e.target.value)}
-                  disabled={isMinting}
-                />
-              </div>
-              <div className={styles.formGroup}>
-                <p className={styles.formLabel}>Description</p>
-                <textarea
-                  className={cx(styles.formInput, styles.longInput)}
-                  maxLength={120}
-                  placeholder="Description"
-                  value={description}
-                  onChange={e => setDescription(e.target.value)}
-                  disabled={isMinting}
-                />
-              </div>
-
-              {isMinting && (
-                <div>
-                  <Stepper activeStep={currentMintingStep} alternativeLabel>
-                    {mintSteps.map(label => (
-                      <Step key={label}>
-                        <StepLabel>{label}</StepLabel>
-                      </Step>
-                    ))}
-                  </Stepper>
-                </div>
-              )}
-              <div
-                className={cx(
-                  styles.button,
-                  (isMinting || !isWalletConnected || !validateMetadata()) &&
-                    styles.disabled
-                )}
-                onClick={
-                  isMinting || !isWalletConnected || !validateMetadata()
-                    ? null
-                    : mintNFT
-                }
-              >
-                {isMinting ? (
-                  <ClipLoader size="16" color="white"></ClipLoader>
-                ) : (
-                  'Mint'
-                )}
-              </div>
-              <div className={styles.fee}>
-                <InfoIcon />
-                &nbsp;5 FTMs are charged to create a new NFT.
-              </div>
-              <div className={styles.mintStatusContainer}>
-                {lastMintedTkId !== 0 && (
-                  <label className={styles.nftIDLabel}>
-                    You have created an NFT with ID of {lastMintedTkId}
-                  </label>
-                )}
-
-                {lastMintedTnxId !== '' && (
-                  <a
-                    className={styles.tnxAnchor}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`https://ftmscan.com/tx/${lastMintedTnxId}`}
+              </>
+            ) : (
+              <>
+                <div className={styles.uploadtitle}>
+                  Drop files here or&nbsp;
+                  <span
+                    className={styles.browse}
+                    onClick={() => imageRef.current?.click()}
                   >
-                    You can track the last transaction here ...
-                  </a>
-                )}
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className={styles.warning}>
-            <WarningIcon className={styles.warningIcon} />
-            <div className={styles.warningTitle}>
-              Can not edit image on small screens!
-            </div>
-            <div className={styles.switchButton} onClick={() => setMode(0)}>
-              Switch Mode
-            </div>
+                    browse
+                  </span>
+                </div>
+                <div className={styles.uploadsubtitle}>
+                  JPG, PNG, BMP, GIF Max 15mb.
+                </div>
+              </>
+            )}
           </div>
-        )}
+        </div>
+        <div className={styles.panel}>
+          <div className={styles.formGroup}>
+            <p className={styles.formLabel}>Name</p>
+            <input
+              className={styles.formInput}
+              maxLength={20}
+              placeholder="Name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              disabled={isMinting}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <p className={styles.formLabel}>Symbol</p>
+            <input
+              className={styles.formInput}
+              maxLength={20}
+              placeholder="Symbol"
+              value={symbol}
+              onChange={e => setSymbol(e.target.value)}
+              disabled={isMinting}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <p className={styles.formLabel}>Description</p>
+            <textarea
+              className={cx(styles.formInput, styles.longInput)}
+              maxLength={120}
+              placeholder="Description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              disabled={isMinting}
+            />
+          </div>
+
+          {isMinting && (
+            <div>
+              <Stepper activeStep={currentMintingStep} alternativeLabel>
+                {mintSteps.map(label => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </div>
+          )}
+          <div
+            className={cx(
+              styles.button,
+              (isMinting || !isWalletConnected || !validateMetadata()) &&
+                styles.disabled
+            )}
+            onClick={
+              isMinting || !isWalletConnected || !validateMetadata()
+                ? null
+                : mintNFT
+            }
+          >
+            {isMinting ? (
+              <ClipLoader size="16" color="white"></ClipLoader>
+            ) : (
+              'Mint'
+            )}
+          </div>
+          <div className={styles.fee}>
+            <InfoIcon />
+            &nbsp;5 FTMs are charged to create a new NFT.
+          </div>
+          <div className={styles.mintStatusContainer}>
+            {lastMintedTkId !== 0 && (
+              <label className={styles.nftIDLabel}>
+                You have created an NFT with ID of {lastMintedTkId}
+              </label>
+            )}
+
+            {lastMintedTnxId !== '' && (
+              <a
+                className={styles.tnxAnchor}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://ftmscan.com/tx/${lastMintedTnxId}`}
+              >
+                You can track the last transaction here ...
+              </a>
+            )}
+          </div>
+        </div>
       </div>
-      <MintModeModal
-        visible={modeSelecting}
-        onModeSelect={_mode => {
-          setMode(_mode);
-          setModeSelecting(false);
-        }}
-      />
     </div>
   );
 };
