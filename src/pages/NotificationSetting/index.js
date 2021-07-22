a55b0230d88927b60c7dc62ab0fc269e59b78f06
akame007 <acame007@gmail.com>
2021-07-23 01:13:31 +1000

    notification settings

--- src/pages/NotificationSetting/index.js
+++ src/pages/NotificationSetting/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import cx from 'classnames';
 import { ClipLoader } from 'react-spinners';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { useWeb3React } from '@web3-react/core';
 
 import HeaderActions from 'actions/header.actions';
 import Header from 'components/header';
+import { useApi } from 'api';
+import { getSigner } from 'contracts';
+import toast from 'utils/toast';
 
 import styles from './styles.module.scss';
 
@@ -106,11 +110,16 @@ const CustomCheckbox = withStyles({
   checked: {},
 })(props => <Checkbox color="default" {...props} />);
 
-const SettingOption = ({ name, label, checked, onChange }) => (
+const SettingOption = ({ name, label, checked, disabled, onChange }) => (
   <FormControlLabel
     className={cx(styles.formControl, checked && styles.selected)}
     control={
-      <CustomCheckbox checked={checked} onChange={onChange} name={name} />
+      <CustomCheckbox
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+        name={name}
+      />
     }
     label={label}
   />
@@ -119,18 +128,73 @@ const SettingOption = ({ name, label, checked, onChange }) => (
 const NotificationSetting = () => {
   const dispatch = useDispatch();
 
+  const { account } = useWeb3React();
+  const {
+    getNonce,
+    getNotificationSettings,
+    updateNotificationSettings,
+  } = useApi();
+  const { authToken } = useSelector(state => state.ConnectWallet);
+
   const [saving, setSaving] = useState(false);
+  const [settings, setSettings] = useState({});
+
+  const getSettings = async () => {
+    const { data } = await getNotificationSettings(authToken);
+    setSettings(data);
+  };
 
   useEffect(() => {
     dispatch(HeaderActions.toggleSearchbar(false));
   }, []);
 
-  const handleChange = () => {};
+  useEffect(() => {
+    if (authToken) {
+      getSettings();
+    }
+  }, [authToken]);
+
+  const handleChange = e => {
+    const key = e.target.name;
+    const newSettings = { ...settings };
+    newSettings[key] = !settings[key];
+    setSettings(newSettings);
+  };
 
   const handleSave = async () => {
     setSaving(true);
+    try {
+      let signature;
+      try {
+        const { data: nonce } = await getNonce(account, authToken);
+        const signer = await getSigner();
+        signature = await signer.signMessage(
+          `Approve Signature on Artion.io with nonce ${nonce}`
+        );
+      } catch (err) {
+        toast(
+          'error',
+          'You need to sign the message to be able to update your settings.'
+        );
+        setSaving(false);
+        return;
+      }
+      const { status } = await updateNotificationSettings(
+        settings,
+        authToken,
+        signature
+      );
+      if (status === 'success') {
+        toast('success', 'Notification settings updated!');
+      }
+    } catch (err) {
+      console.log(err);
+    }
     setSaving(false);
   };
+
+  const getSetting = key =>
+    settings[key] === undefined ? true : settings[key];
 
   return (
     <div className={styles.container}>
@@ -144,8 +208,8 @@ const NotificationSetting = () => {
             classes={{ label: styles.groupTitle }}
             control={
               <CustomCheckbox
-                checked={true}
-                onChange={() => {}}
+                checked={getSetting('fNotification')}
+                onChange={handleChange}
                 name="fNotification"
               />
             }
@@ -157,7 +221,10 @@ const NotificationSetting = () => {
                 key={idx}
                 name={option.value}
                 label={option.label}
-                checked={true}
+                disabled={!getSetting('fNotification')}
+                checked={
+                  getSetting('fNotification') && getSetting(option.value)
+                }
                 onChange={handleChange}
               />
             ))}
@@ -168,7 +235,13 @@ const NotificationSetting = () => {
           <FormControlLabel
             className={cx(styles.formControl, styles.selected)}
             classes={{ label: styles.groupTitle }}
-            control={<CustomCheckbox onChange={() => {}} name="buyNow" />}
+            control={
+              <CustomCheckbox
+                checked={getSetting('sNotification')}
+                onChange={handleChange}
+                name="sNotification"
+              />
+            }
             label="Follower Activity Notifications"
           />
           <div className={styles.groupOptions}>
@@ -177,7 +250,10 @@ const NotificationSetting = () => {
                 key={idx}
                 name={option.value}
                 label={option.label}
-                checked={true}
+                disabled={!getSetting('sNotification')}
+                checked={
+                  getSetting('sNotification') && getSetting(option.value)
+                }
                 onChange={handleChange}
               />
             ))}
