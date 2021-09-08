b353923523a14403587dbcbc01fe4fbf3692ff86
okura <okura@fantom.foundation>
2021-09-08 12:38:39 +1000

    display car to gasly page

--- src/components/Gasly/Home/index.js
+++ src/components/Gasly/Home/index.js
@@ -7,6 +7,7 @@ import avatar from 'assets/imgs/gasly.png';
 import sign from 'assets/imgs/sign.png';
 import logo from 'assets/imgs/logo.png';
 import textBg1 from 'assets/imgs/text_bg1.png';
+import car from 'assets/imgs/car.png';
 
 import styles from './styles.module.scss';
 
@@ -32,8 +33,27 @@ const GaslyHomePage = () => {
               <span>Golden tickets</span>” redeemable for collectible physical
               items.
             </div>
+            <div className={styles.stats}>
+              <div className={styles.stat}>
+                <div className={styles.statLabel}>Wins</div>
+                <div className={styles.statVal}>2</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statLabel}>Podiums</div>
+                <div className={styles.statVal}>5</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statLabel}>Fastest Laps</div>
+                <div className={styles.statVal}>1</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statLabel}>Pole</div>
+                <div className={styles.statVal}>1</div>
+              </div>
+            </div>
           </div>
         </div>
+        <img src={car} alt="car" className={styles.car} />
       </div>
     </div>
   );
