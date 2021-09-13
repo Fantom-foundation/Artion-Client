6a88fe7489557083b4a520a301d03d276d8797a5
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-09-13 13:44:08 +0400

    Fix Share on twitter

--- src/pages/NFTItem/index.js
+++ src/pages/NFTItem/index.js
@@ -2286,7 +2286,7 @@ const NFTItem = () => {
   const handleShareToTwitter = () => {
     handleClose();
     window.open(
-      `https://twitter.com/intent/tweet?text=Check%20out%20this%item%20on%20Artion&url=${window.location.href}`,
+      `https://twitter.com/intent/tweet?text=Check%20out%20this%20item%20on%20Artion&url=${window.location.href}`,
       '_blank'
     );
   };
