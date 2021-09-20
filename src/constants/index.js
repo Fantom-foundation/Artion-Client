523dd7d41b86309a1a1664d75c551024078ef1e1
javamusat <javamusat@gmail.com>
2021-09-20 08:45:41 +0200

    [admin] change hardcoded address

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const NetworkContextName = 'NETWORK';
 export const FTM_TOTAL_SUPPLY = 3175000000;
-export const ADMIN_ADDRESS = '0xB7bC6D2666e73F8Cd143a929DB5404e2fc03eA89';
+export const ADMIN_ADDRESS = '0x2FDf27b6985331F7d8C30dE479baD4F184E0d8cB';
