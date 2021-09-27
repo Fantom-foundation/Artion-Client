23c70498241551ab6cb0d9343d81dba4545014e9
javamusat <javamusat@gmail.com>
2021-09-27 20:16:53 +0200

    [set] admin to contract owner

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const NetworkContextName = 'NETWORK';
 export const FTM_TOTAL_SUPPLY = 3175000000;
-export const ADMIN_ADDRESS = '0xB7bC6D2666e73F8Cd143a929DB5404e2fc03eA89';
+export const ADMIN_ADDRESS = '0x1bffb3a232e06e06a5d9e93c8df3321f768197c2';
